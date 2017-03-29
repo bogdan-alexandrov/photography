7f96eb2876faa6e6546320854b351c80963ee557
ALEXANDB <aleksandrov.bogdan@gmail.com>
2017-03-29 15:37:54 +0200

    pagination

--- public/js/main.js
+++ public/js/main.js
@@ -76,7 +76,7 @@ jQuery(function () {
                     pagination: '.load-more',
                     next: '.nextPage',
                     negativeMargin: 200,
-                    delay: 0
+                    delay: 700
                 });
 
                 var $grid = $('.masonry');
@@ -86,7 +86,17 @@ jQuery(function () {
                     $grid.masonry('reloadItems');
                     setTimeout(animate, 0);
                 });
-                // /AJAX loading
+
+                // Add a loader image which is displayed during loading
+                ias.extension(new IASSpinnerExtension({
+                        html: '<i class="fa fa-2x fa-spin fa-spinner" aria-hidden="true"></i>'
+                    }
+                ));
+                // Add a link after page 2 which has to be clicked to load the next page
+                // ias.extension(new IASTriggerExtension({offset: 1}));
+                // // Add a text when there are no more pages left to load
+                // ias.extension(new IASNoneLeftExtension({text: "You reached the end"}));
+
 
             });
 
